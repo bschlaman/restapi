e043c42854488d3f126822864b8b3cdab243ad10
Brendan Schlaman <brendan.schlaman@gmail.com>
2021-01-21 20:50:48 +0000

    start

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 PID=`pgrep node`
 [ $? -eq 0 ] && { echo PID: $PID.  Killing and starting new instance... ; kill $PID ; } \
 	|| echo No instace was up. Starting...
-#nohup node app.js > /dev/null 2>&1 &
-#node app.js > stdout 2>&1 &
 [ -f ./nohup.out ] && rm ./nohup.out
-nohup node server.js &
+nohup node server.js & 2>&1
